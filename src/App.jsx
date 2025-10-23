3ab14ba18c396737d23ff50cf57d82f5247d10ff
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-10-23 14:44:18 -0300

    style: add sucess register page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Marketplace } from './pages/Marketplace'
 import { LandingPage } from './pages/LandingPage/LandingPage'
 import { Login } from './pages/SignIn/SignIn'
 import { Register } from './pages/Register/Register'
+import { Success } from './pages/Success/Success'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route path="/home" element={<LandingPage />} />
       <Route path="/home/login" element={<Login />} />
       <Route path="/home/register" element={<Register />} />
+      <Route path="/home/register/success" element={<Success />} />
     </Routes>
   )
 }
---
+++ src/pages/Success/Success.jsx
@@ -0,0 +1,15 @@
+import './success.style.css'
+
+import { Button } from '../../components/Button/Button'
+
+export function Success(){
+    return (
+        <main className="success-container">
+            <div className="success-content-container">
+                <h2 className='Principal_text_introduction'>Your account has been <br /> successfully created!</h2>
+                <p className='Secondary_text_introduction'>Time to explore and complete your Poké-collection</p>
+                <Button text="Catch ’Em Now" type="secondary" />
+            </div>
+        </main>
+    )
+}
