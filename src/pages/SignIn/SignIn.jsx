266c4aa377d7408e5e8b3710f0a5d7b6b25c3e3c
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-10-23 14:31:44 -0300

    style: add background img in the login page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,10 @@
 import './signin.style.css'
 import { Button } from "../../components/Button/Button";
+import { Input } from '../../components/Input/Input';
 import { useNavigate } from "react-router-dom";
+
+import { FiMail } from "react-icons/fi";
+import { FiLock } from "react-icons/fi";
 
 export function Login(){
     const navigate = useNavigate();
@@ -14,13 +18,15 @@ export function Login(){
             <div className="login-container">
                 <div className="login-left-col">
                     <h2 className='login-title'>Sign In to Pokedecks</h2>
-                    {/* <Input name value/>
-                    <Input name value/> */}
+                    <div className="Register_information_container">
+                        <Input icon={FiMail} id="email" placeholder="E-mail" name="email" type="email" />
+                        <Input icon={FiLock} id="password" placeholder="Password" name="password" type="password" />
+                    </div>
                     <Button text="Sign In" type="primary" onClick={() => handleOtherPage('')} />
                 </div>
                 <div className="login-right-col">
-                    <h2>Hello, Trainer!</h2>
-                    <p>Enter your pokerecords and start the journey with us</p>
+                    <h1 className="Principal_text_introduction">Hello, Trainer!</h1>
+                    <p className="Secondary_text_introduction">Enter your pokerecords and start the journey with us</p>
                     <Button text="Sign Up" type="secondary" onClick={() => handleOtherPage('/home/register')}/>
                 </div>
             </div>
