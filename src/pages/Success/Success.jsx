2f36c9fc678202047e5aa3726daca253a65c1562
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-10-29 19:02:51 -0300

    feat: add navigation to button on success page

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,14 +1,18 @@
 import './success.style.css'
 
 import { Button } from '../../components/Button/Button'
+import { useNavigate } from 'react-router-dom'
 
 export function Success(){
+    const navigate = useNavigate();
+
+
     return (
         <main className="success-container">
             <div className="success-content-container">
                 <h2 className='Principal_text_introduction'>Your account has been <br /> successfully created!</h2>
                 <p className='Secondary_text_introduction'>Time to explore and complete your Poké-collection</p>
-                <Button typeColor="secondary">Catch ’Em Now</Button>
+                <Button typeColor="secondary" onClick={() => navigate('/')}>Catch ’Em Now</Button>
             </div>
         </main>
     )
