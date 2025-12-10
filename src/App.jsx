17d2c4b96b631e782bc30c0646339299669d7816
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-12-09 21:10:33 -0300

    refactor: update marketplace import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
 
-import { Marketplace } from './pages/Marketplace'
+import { Marketplace } from './pages/Marketplace/Marketplace'
 import { LandingPage } from './pages/LandingPage/LandingPage'
 import { Login } from './pages/SignIn/SignIn'
 import { Register } from './pages/Register/Register'
