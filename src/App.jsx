ffbe503ddb601db75e7a77cc0c89a6658ac702f9
João Pedro Lemos Silveira <lemossilveira.04@gmail.com>
2025-12-15 17:54:06 -0300

    pagina do adm (orders)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
 
 import { Routes, Route } from 'react-router-dom'
-
 import { Marketplace } from './pages/Marketplace/Marketplace'
 import { LandingPage } from './pages/LandingPage/LandingPage'
 import { Login } from './pages/SignIn/SignIn'
 import { Register } from './pages/Register/Register'
 import { Success } from './pages/Success/Success'
 import { Profile } from './pages/Profile/Profile'
+import { Orders } from './pages/adm_Orders/orders'
 
 
 
@@ -16,12 +16,12 @@ function App() {
   return (
     <Routes>
       <Route path="/authentication" element={<LandingPage />} />
-      
       <Route path="/" element={<Marketplace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/register/success" element={<Success />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path='/orders' element={<Orders />}/>
     </Routes>
   )
 }
